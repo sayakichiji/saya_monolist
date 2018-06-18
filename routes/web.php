b80c652038f8f9e382e7b0b0936bfefabd8282db
Saya.Kichiji <ec2-user@ip-172-31-1-248.us-east-2.compute.internal>
2018-06-18 03:57:58 +0000

    rakuten api

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,18 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');});
-    Route::get('/', 'WelcomeController@index');
-    Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+return view('welcome');});
+Route::get('/', 'WelcomeController@index');
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
     // Login authentication
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
-    
+Route::group(['middleware' => ['auth']], function () {
+Route::resource('items', 'ItemsController', ['only' => ['create']]);
+}); 
     
     
     
